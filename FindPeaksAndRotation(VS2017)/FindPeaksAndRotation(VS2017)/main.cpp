1640fde0cec0e180cd1273ffba79b64fa09f6d1e
Hyeonu Choi <renewal1990@gmail.com>
2018-07-24 17:05:44 +0900

    다른 각도 샘플 추가 및 샘플폴더(result) 생성

--- FindPeaksAndRotation(VS2017)/FindPeaksAndRotation(VS2017)/main.cpp
+++ FindPeaksAndRotation(VS2017)/FindPeaksAndRotation(VS2017)/main.cpp
@@ -2,7 +2,7 @@
 
 void main()
 {
-	Mat input, temp, output;
+	Mat input, temp, matrix, output;
 	/*
 	//add peaks
 	input = imread("./image/dodo.png");
@@ -14,14 +14,14 @@ void main()
 	/*
 	//rotation
 	input = imread("./image/dodo_peaks_added.png");
-	Mat matrix = getRotationMatrix2D(Point2f(input.cols / 2, input.rows / 2), 45, 1);
+	matrix = getRotationMatrix2D(Point2f(input.cols / 2, input.rows / 2), 20, 1);
 	warpAffine(input, output, matrix, Size(input.cols, input.rows));
-	imwrite("./image/dodo_peaks_added_rotate_45.png", output);
+	imwrite("./image/dodo_peaks_added_rotate_20.png", output);
 	*/
 
 	input = imread("./image/dodo_peaks_added_rotate_20.png");
 	FindPeaks fp(&input);
-	Mat matrix = getRotationMatrix2D(Point2f(input.cols / 2, input.rows / 2), fp.angle, fp.scale);
+	matrix = getRotationMatrix2D(Point2f(input.cols / 2, input.rows / 2), fp.angle, fp.scale);
 	warpAffine(input, output, matrix, Size(input.cols, input.rows));
 	imwrite("./image/output.png", output);	
 }
